7e85fecf0ff666dc360421265a1561c00c478ef6
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2021-05-11 15:49:16 -0300

    Drawer Layout Config inserted.

--- app/src/main/java/com/example/roliudi/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/roliudi/ui/main/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.roliudi.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.roliudi.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+
     }
 }
