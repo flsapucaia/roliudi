c2dbcddbcd03ea430fc41d87402c71403f725804
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2021-05-11 18:01:27 -0300

    Navigation Inserted.

--- app/src/main/java/com/example/roliudi/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/roliudi/ui/main/MainActivity.kt
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.roliudi.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +24,22 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_movies,
+                R.id.nav_about
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 }
