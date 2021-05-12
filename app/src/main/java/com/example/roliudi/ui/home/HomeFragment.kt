a16453ea0bc5245ac6ace67b341a5ddbab444992
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2021-05-12 18:01:42 -0300

    Navigation with Drawer Layout and files.

--- app/src/main/java/com/example/roliudi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/roliudi/ui/home/HomeFragment.kt
@@ -1,4 +1,19 @@
 package com.example.roliudi.ui.home
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.roliudi.R
+
+class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 }
