a07afb279557f113463dc32294f9bd1f1b852b92
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2021-06-29 22:24:53 -0300

    DomainModule.kt updated

--- app/src/main/java/com/example/roliudi/domain/DomainModule.kt
+++ app/src/main/java/com/example/roliudi/domain/DomainModule.kt
@@ -1,13 +1,15 @@
 package com.example.roliudi.domain
 
+import com.example.roliudi.data.remote.datasource.IUserAuthenticationDataSource
+import com.example.roliudi.data.remote.datasource.UserAuthenticationDataSource
 import com.example.roliudi.domain.repository.userAuth.IUserAuthRepository
 import com.example.roliudi.domain.repository.userAuth.UserAuthRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val domainModule = module {
-    single {
-        androidContext()
+    single<IUserAuthenticationDataSource> {
+        UserAuthenticationDataSource(get())
     }
 
     single<IUserAuthRepository> {
